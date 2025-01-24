110b8b8d1f7dad50269129d161d50cdfe686f608
gioprana89 <gioprana89@gmail.com>
2025-01-24 22:08:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ st.write('''<br><br><br><center><font color = "#0000ff" size = 7>APLIKASI PYTHON
 
 
 
-st.write('''<br><left><font color = "#ff3814" size = 3>Aplikasi berikut menampilkan data tabel yang diimport dari data excel. Data yang disajikan membuat link, yang apabila di-click, akan menuju ke suatu alamat website</font><font color = "blue" style="background-color: #d0ff14"><a href = "https://github.com/gioprana89/2025_galeri_streamlit_datatabel2" target = '_blank' style = 'text-decoration:none'> Download Kode.</a></font>
+st.write('''<br><left><font color = "#ff3814" size = 3>Aplikasi berikut menampilkan data tabel yang diimport dari data excel. Data yang disajikan memuat link, yang apabila di-click, akan menuju ke suatu alamat website</font><font color = "blue" style="background-color: #d0ff14"><a href = "https://github.com/gioprana89/2025_galeri_streamlit_datatabel2" target = '_blank' style = 'text-decoration:none'> Download Kode.</a></font>
 
 
 
